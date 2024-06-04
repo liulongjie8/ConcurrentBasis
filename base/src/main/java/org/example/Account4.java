b8969706e89340732921442123f580dcea6251ed
liulongjie8 <1942625600@qq.com>
2024-06-04 16:39:09 +0800

    first commit

--- base/src/main/java/org/example/Account4.java
+++ base/src/main/java/org/example/Account4.java
@@ -9,12 +9,18 @@ public class Account4 {
     public void transfer(Account4 target, int amt ){
         while(allocator.apply(this,target));
         try {
-             //锁定
+             //锁定转出账户
              synchronized (this){
-
+                 //锁定转入账户
+                 synchronized (target){
+                     if (this.balance > amt) {
+                         this.balance -= amt;
+                         target.balance += amt;
+                     }
+                 }
              }
         }catch (Exception e){
-
+            e.printStackTrace();
         }finally {
             allocator.free(this,target);
         }
